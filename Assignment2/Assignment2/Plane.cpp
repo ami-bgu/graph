6dd8815e4b11dd2e05792e72293a10d29d6a4971
Ami <amioren10@gmail.com>
2015-04-25 16:23:35 +0300

    plane fixed

--- Assignment2/Assignment2/Plane.cpp
+++ Assignment2/Assignment2/Plane.cpp
@@ -31,28 +31,27 @@ RayHitData Plane::getRayHitResult(const Vector3f& source, const Vector3f& vec, A
 	//calculate the point of impact in the infinite plane
 	Vector3f& q0 = this->center;
 	const Vector3f& p0 = source;
-	Vector3f p = p0 + vec * Vector3f::dotProduct(this->normal, (q0 - p0) / Vector3f::dotProduct(this->normal, vec));
+	Vector3f p = p0 + (vec * Vector3f::dotProduct(this->normal, (q0 - p0) / Vector3f::dotProduct(this->normal, vec)));
 	//todo: check what happen if the plane is parallel 
 
-	rhd.isHit = true;
+
+	
 	int arr[8] = { 0, 1, 1, 2, 2, 3, 3, 0 };
 
 	for (int i = 0; i < 8; i+=2){
-		Vector3f V1 = corners[i] - source;
-		Vector3f V2 = corners[1+1] - source;
-		Vector3f V1xV2 = Vector3f::crossProduct(V1, V2);
-
-		Vector3f N = V1xV2 / V1xV2.getLength();
-		Vector3f minusv = (p - p0);
-		if (Vector3f::dotProduct(minusv, N) < 0){
+		Vector3f V1 = corners[arr[i]] - source;
+		Vector3f V2 = corners[arr[i + 1]] - source;
+		Vector3f triangleNormal = Vector3f::crossProduct(V1, V2);
+		triangleNormal.normalize();
+		if (Vector3f::dotProduct(p - p0, triangleNormal) < 0){
 			rhd.isHit = false;
 			return rhd;
 		}
 		
 	}
-
-	//if (!rhd.isHit) return rhd;
-
+	
+	//rhd.
+	rhd.isHit = true;
 	rhd.pointOfHit = p;
 	rhd.distance = (p - p0).getLength();
 	rhd.intensity = Shape::calculateIntensity(p, ambient, lights, shapes);
@@ -79,14 +78,15 @@ float Plane::rayHitDistance(const Vector3f& source, const Vector3f& vec)
 	int arr[8] = { 0, 1, 1, 2, 2, 3, 3, 0 };
 
 	for (int i = 0; i < 8; i += 2){
-		Vector3f V1 = corners[i] - source;
-		Vector3f V2 = corners[1 + 1] - source;
-		Vector3f V1xV2 = Vector3f::crossProduct(V1, V2);
-		Vector3f N = V1xV2 / V1xV2.getLength();
-
-		if (Vector3f::dotProduct((p - p0), N) >= 0){
-			rhd.isHit = true;
+		Vector3f V1 = corners[arr[i]] - source;
+		Vector3f V2 = corners[arr[i + 1]] - source;
+		Vector3f triangleNormal = Vector3f::crossProduct(V1, V2);
+		triangleNormal.normalize();
+		if (Vector3f::dotProduct(p - p0, triangleNormal) < 0){
+			rhd.isHit = false;
+			return 0;
 		}
+
 	}
 
 	if (!rhd.isHit) return 0;
@@ -108,6 +108,6 @@ void Plane::calculateCorners()
 
 	corners[0] = center + (horizontalDirection + verticalDirection);
 	corners[1] = center + ( (horizontalDirection*(-1)) + verticalDirection);
-	corners[2] = center + (horizontalDirection + (verticalDirection*(-1)) );
-	corners[3] = center + ( (horizontalDirection*(-1)) + (verticalDirection*(-1)) );
+	corners[2] = center + ((horizontalDirection*(-1)) + (verticalDirection*(-1)));
+	corners[3] = center + (horizontalDirection + (verticalDirection*(-1)) );
 }
