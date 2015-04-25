f8dc1e0221e2503d55af8f2fbc7bb2e4040aa8bf
Ami <amioren10@gmail.com>
2015-04-25 14:17:07 +0300

    broken plane

--- Assignment2/Assignment2/Plane.cpp
+++ Assignment2/Assignment2/Plane.cpp
@@ -32,12 +32,31 @@ RayHitData Plane::getRayHitResult(const Vector3f& source, const Vector3f& vec, A
 	Vector3f& q0 = this->center;
 	const Vector3f& p0 = source;
 	Vector3f p = p0 + vec * Vector3f::dotProduct(this->normal, (q0 - p0) / Vector3f::dotProduct(this->normal, vec));
-	
-	//check if point of impact is in finite plane
-	//TODO: finish this code
+	//todo: check what happen if the plane is parallel 
 
+	rhd.isHit = true;
+	int arr[8] = { 0, 1, 1, 2, 2, 3, 3, 0 };
 
-	rhd.isHit = false;
+	for (int i = 0; i < 8; i+=2){
+		Vector3f V1 = corners[i] - source;
+		Vector3f V2 = corners[1+1] - source;
+		Vector3f V1xV2 = Vector3f::crossProduct(V1, V2);
+
+		Vector3f N = V1xV2 / V1xV2.getLength();
+		Vector3f minusv = (p - p0);
+		if (Vector3f::dotProduct(minusv, N) < 0){
+			rhd.isHit = false;
+			return rhd;
+		}
+		
+	}
+
+	//if (!rhd.isHit) return rhd;
+
+	rhd.pointOfHit = p;
+	rhd.distance = (p - p0).getLength();
+	rhd.intensity = Shape::calculateIntensity(p, ambient, lights, shapes);
+
 	return rhd;
 }
 
@@ -48,8 +67,33 @@ Vector3f Plane::getNormal(const Vector3f& point)
 
 float Plane::rayHitDistance(const Vector3f& source, const Vector3f& vec)
 {
-	//TODO: implement
-	return -1;
+	//todo: simplifiy .. this is a copy paste..
+	RayHitData rhd;
+	//calculate the point of impact in the infinite plane
+	Vector3f& q0 = this->center;
+	const Vector3f& p0 = source;
+	Vector3f p = p0 + vec * Vector3f::dotProduct(this->normal, (q0 - p0) / Vector3f::dotProduct(this->normal, vec));
+	//todo: check what happen if the plane is parallel 
+
+	rhd.isHit = false;
+	int arr[8] = { 0, 1, 1, 2, 2, 3, 3, 0 };
+
+	for (int i = 0; i < 8; i += 2){
+		Vector3f V1 = corners[i] - source;
+		Vector3f V2 = corners[1 + 1] - source;
+		Vector3f V1xV2 = Vector3f::crossProduct(V1, V2);
+		Vector3f N = V1xV2 / V1xV2.getLength();
+
+		if (Vector3f::dotProduct((p - p0), N) >= 0){
+			rhd.isHit = true;
+		}
+	}
+
+	if (!rhd.isHit) return 0;
+
+	rhd.distance = (p - p0).getLength();
+
+	return rhd.distance;
 }
 
 void Plane::calculateCorners()
@@ -57,4 +101,13 @@ void Plane::calculateCorners()
 	Vector3f horizontalDirection;
 	Vector3f verticalDirection;
 	normal.getTwoOrthogonals(horizontalDirection, verticalDirection);
+	horizontalDirection.normalize();
+	verticalDirection.normalize();
+	horizontalDirection *= width / 2;
+	verticalDirection *= height / 2;
+
+	corners[0] = center + (horizontalDirection + verticalDirection);
+	corners[1] = center + ( (horizontalDirection*(-1)) + verticalDirection);
+	corners[2] = center + (horizontalDirection + (verticalDirection*(-1)) );
+	corners[3] = center + ( (horizontalDirection*(-1)) + (verticalDirection*(-1)) );
 }
